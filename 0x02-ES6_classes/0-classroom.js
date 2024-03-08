0b4623f4348bb5f64e71b82120902ea4ccf00315
elassari19 <elassari19@gmail.com>
2024-03-08 01:10:59 +0100

    0 classroom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+// defines a class named ClassRoom
+
+export default class ClassRoom {
+  // constructor method
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
