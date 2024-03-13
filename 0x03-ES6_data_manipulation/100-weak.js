fd70fe6cf672886fd25df1a5a59be8e4c163e259
elassari19 <elassari19@gmail.com>
2024-03-13 23:46:44 +0000

    0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,8 @@
 // should be able to use WeakMap
 
 export const queryAPI = (endpoint) => {
+  const weakMap = new WeakMap();
+
   let total = weakMap.get(endpoint) || 0;
   weakMap.set(endpoint, total -= -1);
   if (total >= 5) throw new Error('Endpoint load is high');
